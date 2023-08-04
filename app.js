3ea3a6e453e1c64cd3699721742a33ca3b2a1297
Nedoengineerka <theriddler2417@gmail.com>
2023-08-04 17:56:19 +0300

    fix(server): change server creation method, add nodemon

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+
 const app = express();
 
-const server = require('http');
-require('dotenv').config();
-
+// middlewares
 app.use(express.json());
+app.use(express.urlencoded());
 
+// create server
 const port = process.env.PORT || 3000;
-server.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
-}).listen(port);
+app.listen(port);
 
 console.log(`Server running at port ${port}`);
